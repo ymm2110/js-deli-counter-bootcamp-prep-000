20628a6a173a42eab282943c25c5ce66c12f9f55
Yuntong Man <yuntongman17@gmail.com>
2018-10-12 18:34:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli = [];
 
-function takeANumber(katzDeliLine, name) {
+function takeANumber(katzDeliLine) {
   katzDeliLine.push(name)
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
